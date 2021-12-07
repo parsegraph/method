0814628cb1a74d3f8893f8f613d38cc72fdd41d2
Aaron Faanes <dafrito@gmail.com>
2021-12-06 18:13:09 -0600

    Upgrade microproject

--- src/method.ts
+++ src/method.ts
@@ -1,17 +1,17 @@
 export default class Method {
-  _func:Function;
-  _funcThisArg:any;
+  _func: Function;
+  _funcThisArg: any;
 
-  constructor(func:Function, funcThisArg?:any) {
+  constructor(func: Function, funcThisArg?: any) {
     this._func = func;
     this._funcThisArg = funcThisArg;
   }
 
-  call(...args:Array<any>) {
+  call(...args: Array<any>) {
     return this._func.apply(this._funcThisArg, args);
-  };
+  }
 
-  apply(args:Array<any>):any {
+  apply(args: Array<any>): any {
     return this._func.apply(this._funcThisArg, args);
-  };
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,20 +1,19 @@
 var assert = require("assert");
 import Method from "../dist/method";
 
-function testMethod(a, b, c)
-{
+function testMethod(a, b, c) {
   return this.base + a + b + c;
 }
 
 describe("Method", function () {
-  it("works with call", ()=>{
-    var b = {base:1};
+  it("works with call", () => {
+    var b = { base: 1 };
     var m = new Method(testMethod, b);
-    assert.equal(1+2+3+4, m.call(2, 3, 4));
+    assert.equal(1 + 2 + 3 + 4, m.call(2, 3, 4));
   });
-  it("works with apply", ()=>{
-    var b = {base:1};
+  it("works with apply", () => {
+    var b = { base: 1 };
     var m = new Method(testMethod, b);
-    assert.equal(1+2+3+4, m.apply([2, 3, 4]));
+    assert.equal(1 + 2 + 3 + 4, m.apply([2, 3, 4]));
   });
 });
