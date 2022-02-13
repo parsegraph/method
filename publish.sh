86a5826cb33ec66f67ade32026cd4a3c758f1be0
Aaron Faanes <dafrito@gmail.com>
2022-02-13 01:19:52 -0600

    Be sure to tag=dev the dev package

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for try in `seq 1 3`; do
-    yarn publish parsegraph-method-dev.tgz --no-git-tag-version --prepatch --preid "$*" && exit
+    yarn publish parsegraph-method-dev.tgz --no-git-tag-version --prepatch --preid "$*" --tag=dev && exit
     echo "$try. Retrying..."
     sleep 3
 done
