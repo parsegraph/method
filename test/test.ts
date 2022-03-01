9a4360f53c4d99b31ccb791d000a632095d9be3d
Aaron Faanes <dafrito@gmail.com>
2022-02-28 22:58:11 -0600

    minor lint

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-var { assert } = require("chai");
+const { assert } = require("chai");
 import Method from "..";
 
 function testMethod(a, b, c) {
@@ -11,24 +11,24 @@ function testMethodB(a, b, c) {
 
 describe("Method", function () {
   it("works with call", () => {
-    var b = { base: 1 };
-    var m = new Method(testMethod, b);
+    const b = { base: 1 };
+    const m = new Method(testMethod, b);
     assert.equal(1 + 2 + 3 + 4, m.call(2, 3, 4));
   });
   it("works with apply", () => {
-    var b = { base: 1 };
-    var m = new Method(testMethod, b);
+    const b = { base: 1 };
+    const m = new Method(testMethod, b);
     assert.equal(1 + 2 + 3 + 4, m.apply([2, 3, 4]));
   });
   it("works with set", () => {
-    var b = { base: 1 };
-    var m = new Method(testMethod, b);
+    const b = { base: 1 };
+    const m = new Method(testMethod, b);
     m.set(testMethodB, b);
     assert.equal(1 - 2 - 3 - 4, m.apply([2, 3, 4]));
   });
   it("works with canCall", () => {
-    var b = { base: 1 };
-    var m = new Method();
+    const b = { base: 1 };
+    const m = new Method();
     assert.isFalse(m.canCall());
     m.call();
     m.set(testMethodB, b);
@@ -36,7 +36,7 @@ describe("Method", function () {
     assert.equal(1 - 2 - 3 - 4, m.apply([2, 3, 4]));
   });
   it("works with returning function", () => {
-    var m = new Method(() => {
+    const m = new Method(() => {
       return true;
     });
     m.clear();
